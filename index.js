0b275b06848f5794b089440b0e05c17398324e38
zhuqiang <zhuqiang@tidebuy.net>
2017-03-09 17:54:37 +0800

    update V1.0.0

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 ﻿var _start = require('./assets/lib/plugin');
+var Config = require('./assets/lib/config');
 module.exports = {
     book: {
         assets: "./assets",
         css: ["style/plugin.css"]
     },
     hooks: {
+        "init": function () {
+            Config.handlerAll(this);
+        },
         "page": function (page) {
             var bookIns = this;
             _start(bookIns, page);
